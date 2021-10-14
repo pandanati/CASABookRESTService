ca937c2404e08a0d6697b16ad29aef8614d7c9af
pandanati <72080264+pandanati@users.noreply.github.com>
2021-10-14 21:38:43 +0200

    Add project files.

---
+++ CASABookRESTService/Controllers/BooksController.cs
@@ -0,0 +1,96 @@
+﻿using CASABookClassLibrary;
+using CASABookRESTService.Managers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CASABookRESTService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksManager _manager = new BooksManager();
+
+        // GET: api/<ValuesController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> Get()
+        {
+            //In case the user doesn't write anything in the URL
+            IEnumerable<Book> books = _manager.GetAllBooks(null);
+            
+            if (books == null)
+            {
+                return NotFound("Could not find any books");
+            }
+
+            return Ok(books);
+        }
+
+        // GET api/<ValuesController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{substring}")]
+        public ActionResult<IEnumerable<Book>> Get(string substring)
+        {
+            IEnumerable<Book> books = _manager.GetAllBooks(substring);
+
+            if (books == null)
+            {
+                return NotFound("Could not find any books");
+            }
+
+            return Ok(books);
+        }
+
+        // POST api/<ValuesController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPost]
+        public ActionResult<Book> Post([FromBody] Book value)
+        {
+            if (value == null)
+            {
+                return NotFound("The value is null");
+            }
+            _manager.Add(value);
+            return Ok();
+        }
+
+        // PUT api/<ValuesController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPut("{isbn}")]
+        public ActionResult<Book> Put(string isbn, [FromBody] Book newBook)
+        {
+            if (newBook == null)
+            {
+                return NotFound("The value is null");
+            }
+            _manager.Update(isbn, newBook);
+            return Ok();
+        }
+
+        // DELETE api/<ValuesController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{isbn}")]
+        public ActionResult<Book> Delete(string isbn)
+        {
+            if (isbn == null)
+            {
+                return NotFound("Book with this ISBN not found");
+            }
+            _manager.Delete(isbn);
+            return Ok();
+        }
+    }
+}
---
+++ CASABookRESTService/Managers/BooksManager.cs
@@ -0,0 +1,56 @@
+﻿using CASABookClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CASABookRESTService.Managers
+{
+    public class BooksManager
+    {
+        private static readonly List<Book> bookList1 = new List<Book>()
+        {
+            new Book(){Author = "Natalie", ISBN = "123ABC", PageNumber = 5, Title = "Ohh"},
+            new Book(){Author = "Pedro", ISBN = "1234ABCD", PageNumber = 102, Title = "Noo"},
+            new Book(){Author = "Tom", ISBN = "ABCD1234", PageNumber = 105, Title = "The Way Of An Alien"}
+        };
+
+
+        public List<Book> GetAllBooks(string substring)
+        {
+            List<Book> resultat = new List<Book>(bookList1);
+            if (substring != null)
+            {
+                resultat = resultat.FindAll(x => x.Title.Contains(substring, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return resultat;
+        }
+
+        public void Add(Book newBook)
+        {
+            bookList1.Add(newBook);
+        }
+
+        public void Delete(string isbn)
+        {
+            Book book = bookList1.Find(x => x.ISBN == isbn);
+            bookList1.Remove(book);
+        }
+
+        public void Update(string isbn, Book update)
+        {
+            Book book = bookList1.Find(x => x.ISBN == isbn);
+            book.Author = update.Author;
+            book.PageNumber = update.PageNumber;
+            book.Title = update.Title;
+        }
+
+
+
+
+
+
+
+    }
+}
